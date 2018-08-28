8760a584a5b4b46793e833912e62fd5db2b7f015
Joe Kowal <3199871+JoeKowal@users.noreply.github.com>
2018-08-28 13:30:07 -0400

    Clean up old files and changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import "./App.css";
+import { GoogleApiWrapper } from "google-maps-react";
+import MapContainer from "./MapContainer";
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div id="App">
+        <MapContainer google={this.props.google} />
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyAtJpL5Y60OmzyYz0EwBcJJOljZiqv_NII"
+})(App);
